fc489cbe93366e5d934b3d302aa6990ce35f3f56
Wesley Aspinall <wra427@gmail.com>
2015-10-01 01:08:39 -0400

    have a few problems left...

--- excercises-2.js
+++ excercises-2.js
@@ -2,16 +2,41 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // ---------------------
 
-function max(){
-    //...
+function max(a,b){
+    if (a>b){
+
+      return a;
+
+    } else if (
+      a === b
+
+    ){
+      return "Silly Goose, the two values are equal!";
+
+    } else {
+
+      return b;
+   
+  }   
 }
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // ---------------------
 
-function maxOfThree(){
-    //...
+function maxOfThree(num1,num2,num3){
+  if(num1 < num3 && num2 < num3){
+      return num3;
+
+  } else if(num1 < num2 && num3 < num2){
+      return num2;
+
+  } else if (num2 < num1 && num3 < num1){
+      return num1;
+
+  } else{
+    return "equal"
+  }
 }
 
 // ---------------------
@@ -19,32 +44,49 @@ function maxOfThree(){
 // ---------------------
 
 function isVowel(char){
-    //...
-}
+  if (char.toLowerCase() === "a"){
+    return "True"
+  } else if(char.toLowerCase() === "e"){
+    return "True"
+  } else if(char.toLowerCase() === "u"){
+    return "True"
+  }else if(char.toLowerCase() === "o"){
+    return "True"
+  }else if(char.toLowerCase() === "i"){
+    return "True"
+  } else {
+    return "False"
+  }
+};
 
+
+   
 // ---------------------
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
-function rovarspraket(phrase){
-    //...
-}
+function rovarspraket(string){
+ var phrase=string.split("");
+
+ if ( phrase !== "a" && phrase !== "e" && phrase !== "i" && phrase !== "o" && phrase !=="u"){
+  console.log(phrase);
+ }
+
 
 
 // ---------------------
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function reverse(){
-    //...
-}
+function reverse(string){
+
 
 // ---------------------
 // Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
 // ---------------------
 
 function findLongestWord(words){
-    //...
+    
 }
 
 // ---------------------
@@ -52,7 +94,10 @@ function findLongestWord(words){
 // ---------------------
 
 function filterLongWords(words, i){
-    //...
+   var box = [ ];
+   box.push(words.length > i.length);
+ }
+
 }
 
 // ---------------------
